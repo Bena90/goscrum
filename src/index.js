6563918aeb073e95071c1a435770ce7584d9043a
Bena90 <esteban.benabarre@gmail.com>
2022-05-15 23:08:22 -0300

    feat: basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='goscrum'>
       <Provider store={store}>
         <App />
       </Provider>
--- user.js
+++
@@ -1,9 +0,0 @@
-const user ={
-    'userName': 'Federico',
-    'password': '1234567890',
-    'email': 'federico@gmail.com',
-    'teamID': '9955d45f16sd15fgs-sdfg',
-    'role': 'Team Leader', // => ['Team Leader', 'Team Member']
-    'continent': 'America', // => ['America', 'Europa', 'Otro']
-    'region': 'Otro', // => ['Otro', 'Latam', 'Brasil', 'America del Norte']
-}
