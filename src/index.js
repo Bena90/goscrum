b079e52968886cbee2d16f6abb03787c88eda7da
Bena90 <esteban.benabarre@gmail.com>
2022-05-15 23:51:38 -0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js'
 import App from './App';
@@ -11,10 +11,10 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='goscrum'>
+    <HashRouter basename='goscrum'>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
