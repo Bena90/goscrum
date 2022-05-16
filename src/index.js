2fc9ce0e99d7190230eaa41fda0b9c515b8af909
Bena90 <esteban.benabarre@gmail.com>
2022-05-16 00:01:04 -0300

    HashRouter 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter basename='goscrum'>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
